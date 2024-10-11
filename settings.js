36bfd25802394933cbd5a87d9b64ef03f630f8cd
Astaboi768 <160958706+Astaboi768@users.noreply.github.com>
2024-10-11 08:30:25 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,15 +6,15 @@ You don't have to set this if you deploy using heroku because you can simply set
 const session = process.env.SESSION || '';
 
 const prefix = process.env.PREFIX || '.';
-const mycode = process.env.CODE || "254";
+const mycode = process.env.CODE || "234";
 const author = process.env.STICKER_AUTHOR || 'fortunatus';
-const packname = process.env.PACKNAME || 'dreaded md2 🤖';
-const dev = process.env.DEV || '254114018035';
+const packname = process.env.PACKNAME || 'DEOLA 🤖';
+const dev = process.env.DEV || '2348039896597';
 const DevDreaded = dev.split(",");
-const botname = process.env.BOTNAME || 'DREADED';
+const botname = process.env.BOTNAME || 'DEOLA';
 const mode = process.env.MODE || 'public';
 const gcpresence = process.env.GC_PRESENCE || 'false';
-const antionce = process.env.ANTIVIEWONCE || 'true';
+const antionce = process.env.ANTIVIEWONCE || 'false';
 const sessionName = "session";
 const presence = process.env.WA_PRESENCE || 'online';
 
